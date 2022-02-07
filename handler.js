e0b3eb001ca1def29d7d0460117b797ead70723a
Kuhera <99056424+Kuhera@users.noreply.github.com>
2022-02-08 07:17:00 +0900

    Add files via upload

--- handler.js
+++ handler.js
@@ -77,7 +77,7 @@ module.exports = {
         if (typeof chat !== 'object') global.db.data.chats[m.chat] = {}
         if (chat) {
           if (!('isBanned' in chat)) chat.isBanned = false
-          if (!('welcome' in chat)) chat.welcome = false
+          if (!('welcome' in chat)) chat.welcome = true
           if (!('detect' in chat)) chat.detect = false
           if (!('sWelcome' in chat)) chat.sWelcome = ''
           if (!('sBye' in chat)) chat.sBye = ''
@@ -91,11 +91,11 @@ module.exports = {
           if (!isNumber(chat.expired)) chat.expired = 0
           if (!('getmsg' in chat)) chat.getmsg = false
           if (!('read' in chat)) chat.read = false
-          if (!('stiker' in chat)) chat.stiker = true
-          if (!('viewonce' in chat)) chat.viewonce = true
+          if (!('stiker' in chat)) chat.stiker = false
+          if (!('viewonce' in chat)) chat.viewonce = false
         } else global.db.data.chats[m.chat] = {
           isBanned: false,
-          welcome: false,
+          welcome: true,
           detect: false,
           sWelcome: '',
           sBye: '',
@@ -109,8 +109,8 @@ module.exports = {
           expired: 0,
           getmsg: false,
           read: false,
-          stiker: true,
-          viewonce: true,
+          stiker: false,
+          viewonce: false,
         }
 
         let settings = global.db.data.settings[this.user.jid]
