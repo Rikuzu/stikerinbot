85e0308700fa3ea649b14ce0989d6c2991fe6da1
Kuhera <99056424+Kuhera@users.noreply.github.com>
2022-02-16 16:57:58 +0800

    Update _firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -8,7 +8,7 @@ handler.all = async function (m) {
     await this.sendButton(m.chat, `
 Hai, ${this.ucapan()}
 
-${user.banned ? 'kamu dibanned' : `Nomor ini adalah salah satu bot WhatsApp, bot yang diambil dari stikerin lalu diubah dikit oleh owner Khael\nJika berminat dan ingin bot ini masuk ke grup kalian chat owner Khael => wa.me/32460222094?text=bang+add+botnya+ke+grup+gue+dong+nanti+gw=topup+in (Gratis!!)`}
+${user.banned ? 'kamu dibanned' : `Nomor ini adalah salah satu bot WhatsApp, bot yang diambil dari stikerin lalu diubah dikit oleh owner Khael\nJika berminat dan ingin bot ini masuk ke grup kalian chat owner Khael => wa.me/32460222094?text=bang+add+botnya+ke+grup+gue+dong+nanti+gw+topup+in (Gratis!!)`}
 `.trim(), watermark, user.banned ? 'Pemilik Bot' : 'Menu', user.banned ? ',owner' : ',?', m)
     user.pc = new Date * 1
 }
