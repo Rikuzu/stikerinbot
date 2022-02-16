a7121ef7f1756396fb7aeb534196f9993199e0b3
Kuhera <99056424+Kuhera@users.noreply.github.com>
2022-02-16 17:05:40 +0800

    Update _autorespon.js

--- plugins/_autorespon.js
+++ plugins/_autorespon.js
@@ -56,11 +56,13 @@ https://github.com/ariffb25/stikerinbot
     }
 
     // update status
-    if (new Date() * 1 - set.status > 1000) {
+    if (set.autoupdatestatus) {
+        if (new Date() * 1 - set.status > 1000) {
             let _uptime = process.uptime() * 1000
             let uptime = conn.clockString(_uptime)
             await this.setStatus(`Aktif selama ${uptime} | Mode: ${set.self ? 'Private' : set.group ? 'Hanya Grup' : 'Publik'} | stikerinbot oleh ariffb`).catch(_ => _)
             set.status = new Date() * 1
+        }
     }
 
 }
