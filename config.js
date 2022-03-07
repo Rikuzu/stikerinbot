d39a5e069cdbbf7da62dfc4879e2bbf11c492ca2
Kuhera <99056424+Kuhera@users.noreply.github.com>
2022-03-07 21:16:37 +0900

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@
 let fs = require('fs')
 let chalk = require('chalk')
 
-global.owner = ['32460222094']
+global.owner = ['6282248192917']
 
 global.APIs = { // API Prefix
 	amel: 'https://melcanz.com',
